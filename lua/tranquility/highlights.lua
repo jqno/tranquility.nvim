7b23603e66628027b973117329d09ba28fbab32a
Jan Ouwens <jan.ouwens@gmail.com>
2022-04-30 14:45:23 +0200

    Splits colors so overrides can chose light or dark

--- lua/tranquility/highlights.lua
+++ lua/tranquility/highlights.lua
@@ -4,19 +4,19 @@ local hi = util.hi
 local hilink = util.hilink
 
 local function set_native_syntax(colors, bg)
-    hi('Comment', colors.comment.light, bg)
-    hi('Constant', colors.literal.light, bg)
-    hi('Debug', colors.warning.light, bg)
-    hi('Delimiter', colors.white.dark, bg)
-    hi('Error', colors.error.light, bg)
-    hi('Identifier', colors.white.light, bg)
-    hi('Ignore', colors.white.dark, bg)
-    hi('Operator', colors.operator.dark, bg)
-    hi('Statement', colors.white.dark, bg, mod.bold)
-    hi('Title', colors.literal.light, bg)
-    hi('Todo', colors.comment.light, bg, mod.bold)
-    hi('Type', colors.white.light, bg, mod.bold)
-    hi('Underlined', colors.white.light, bg, mod.underline)
+    hi('Comment', colors.comment, bg)
+    hi('Constant', colors.literal, bg)
+    hi('Debug', colors.warning, bg)
+    hi('Delimiter', colors.dark_white, bg)
+    hi('Error', colors.error, bg)
+    hi('Identifier', colors.light_white, bg)
+    hi('Ignore', colors.dark_white, bg)
+    hi('Operator', colors.operator, bg)
+    hi('Statement', colors.dark_white, bg, mod.bold)
+    hi('Title', colors.literal, bg)
+    hi('Todo', colors.comment, bg, mod.bold)
+    hi('Type', colors.light_white, bg, mod.bold)
+    hi('Underlined', colors.light_white, bg, mod.underline)
 
     hilink('Boolean', 'Constant')
     hilink('Character', 'Constant')
@@ -47,8 +47,8 @@ local function set_visual_elements(colors, bg)
     hilink('Terminal', 'Normal')
     hilink('NonText', 'Ignore')
     hilink('EndOfBuffer', 'Ignore')
-    hi('Conceal', colors.black.light, bg)
-    hi('FloatBorder', colors.ui.light, colors.black.light)
+    hi('Conceal', colors.light_black, bg)
+    hi('FloatBorder', colors.ui, colors.light_black)
 end
 
 local function set_floaterm()
@@ -56,9 +56,9 @@ local function set_floaterm()
 end
 
 local function set_gitsigns(colors, bg)
-    hi('GitSignsAdd', colors.green.light, bg)
-    hi('GitSignsChange', colors.yellow.light, bg)
-    hi('GitSignsDelete', colors.red.light, bg)
+    hi('GitSignsAdd', colors.light_green, bg)
+    hi('GitSignsChange', colors.light_yellow, bg)
+    hi('GitSignsDelete', colors.light_red, bg)
 end
 
 local function set_lsp(colors, bg)
@@ -66,14 +66,14 @@ local function set_lsp(colors, bg)
     hilink('LspReferenceRead', 'DiffAdd')
     hilink('LspReferenceWrite', 'DiffDelete')
 
-    hi('DiagnosticSignError', colors.error.light, bg)
-    hi('DiagnosticSignWarning', colors.warning.light, bg)
-    hi('DiagnosticSignInformation', colors.info.light, bg)
-    hi('DiagnosticSignHint', colors.hint.light, bg)
-    hi('DiagnosticUnderlineError', colors.error.light, bg, mod.underline)
-    hi('DiagnosticUnderlineWarning', colors.warning.light, bg, mod.underline)
-    hi('DiagnosticUnderlineInformation', colors.info.light, bg, mod.underline)
-    hi('DiagnosticUnderlineHint', colors.hint.light, bg, mod.underline)
+    hi('DiagnosticSignError', colors.error, bg)
+    hi('DiagnosticSignWarning', colors.warning, bg)
+    hi('DiagnosticSignInformation', colors.info, bg)
+    hi('DiagnosticSignHint', colors.hint, bg)
+    hi('DiagnosticUnderlineError', colors.error, bg, mod.underline)
+    hi('DiagnosticUnderlineWarning', colors.warning, bg, mod.underline)
+    hi('DiagnosticUnderlineInformation', colors.info, bg, mod.underline)
+    hi('DiagnosticUnderlineHint', colors.hint, bg, mod.underline)
     hilink('DiagnosticVirtualTextError', 'DiagnosticSignError')
     hilink('DiagnosticVirtualTextWarning', 'DiagnosticSignWarning')
     hilink('DiagnosticVirtualTextInformation', 'DiagnosticSignInformation')
@@ -81,9 +81,9 @@ local function set_lsp(colors, bg)
 end
 
 local function set_neovim(colors)
-    hi('healthError', colors.black.light, colors.error.light, mod.bold)
-    hi('healthSuccess', colors.black.light, colors.green.light, mod.bold)
-    hi('healthWarning', colors.black.light, colors.warning.light, mod.bold)
+    hi('healthError', colors.light_black, colors.error, mod.bold)
+    hi('healthSuccess', colors.light_black, colors.green, mod.bold)
+    hi('healthWarning', colors.light_black, colors.warning, mod.bold)
 end
 
 local function set_nvimtree()
@@ -91,8 +91,8 @@ local function set_nvimtree()
 end
 
 local function set_telescope(colors, bg)
-    hi('TelescopeBorder', colors.ui.light, bg)
-    hi('TelescopeMatching', colors.ui.red, bg)
+    hi('TelescopeBorder', colors.ui, bg)
+    hi('TelescopeMatching', colors.ui, bg)
 end
 
 local function set_treesitter(colors, bg)
@@ -149,11 +149,11 @@ local function set_treesitter(colors, bg)
     hilink('TSTypeBuiltin', 'TSType')
     hilink('TSVariableBuiltin', 'TSKeyword')
 
-    hi('TSURI', colors.blue.light, bg)
-    hi('TSText', colors.white.light, bg)
-    hi('TSStrong', colors.white.light, bg, mod.bold)
-    hi('TSEmphasis', colors.white.light, bg, mod.italic)
-    hi('TSStrike', colors.white.dark, bg)
+    hi('TSURI', colors.light_blue, bg)
+    hi('TSText', colors.light_white, bg)
+    hi('TSStrong', colors.light_white, bg, mod.bold)
+    hi('TSEmphasis', colors.light_white, bg, mod.italic)
+    hi('TSStrike', colors.dark_white, bg)
     hilink('TSUnderline', 'Underlined')
     hilink('TSTitle', 'Title')
     hilink('TSMath', 'TSText')
@@ -188,7 +188,7 @@ local function set_filetype_markdown()
 end
 
 local function set_filetype_vimwiki(colors, bg)
-    hi('VimwikiWeblink1', colors.literal.light, bg, mod.underline)
+    hi('VimwikiWeblink1', colors.literal, bg, mod.underline)
 end
 
 local function set_filetype_xml()
@@ -202,15 +202,15 @@ end
 
 local function set_gui(colors, bg)
     -- messages
-    hi('ErrorMsg', colors.white.light, colors.error.dark, mod.bold)
-    hi('WarningMsg', colors.black.light, colors.warning.dark, mod.bold)
-    hi('ModeMsg', colors.ui.dark, bg)
+    hi('ErrorMsg', colors.light_white, colors.error, mod.bold)
+    hi('WarningMsg', colors.light_black, colors.warning, mod.bold)
+    hi('ModeMsg', colors.ui, bg)
     hilink('MoreMsg', 'ModeMsg')
     hilink('Question', 'ModeMsg')
 
     -- statusline and other lines
-    hi('StatusLine', colors.ui.light, colors.white.light)
-    hi('StatusLineNC', colors.white.dark, colors.black.light)
+    hi('StatusLine', colors.ui, colors.light_white)
+    hi('StatusLineNC', colors.dark_white, colors.light_black)
     hilink('StatusLineTerm', 'StatusLine')
     hilink('StatusLineTermNC', 'StatusLineNC')
     hilink('TabLine', 'StatusLineNC')
@@ -218,50 +218,50 @@ local function set_gui(colors, bg)
     hilink('TabLineSel', 'StatusLine')
     hilink('ToolbarButton', 'StatusLine')
     hilink('ToolbarLine', 'StatusLineNC')
-    hi('WildMenu', colors.ui.light, colors.white.light)
-    hi('VertSplit', colors.black.light, colors.black.light)
-    hi('WinSeparator', colors.ui.light, mod.none)
+    hi('WildMenu', colors.ui, colors.light_white)
+    hi('VertSplit', colors.light_black, colors.light_black)
+    hi('WinSeparator', colors.ui, mod.none)
 
     -- cursor
-    hi('ColorColumn', mod.none, colors.ui.dark)
-    hi('CursorColumn', mod.none, colors.black.light)
+    hi('ColorColumn', mod.none, colors.ui)
+    hi('CursorColumn', mod.none, colors.light_black)
     hilink('CursorLine', 'CursorColumn')
-    hi('Cursor', mod.none, colors.ui.dark)
+    hi('Cursor', mod.none, colors.ui)
     hilink('CursorIM', 'Cursor')
     hilink('lCursor', 'Cursor')
-    hi('LineNr', colors.white.dark, bg)
-    hi('CursorLineNr', colors.ui.dark, bg)
+    hi('LineNr', colors.dark_white, bg)
+    hi('CursorLineNr', colors.ui, bg)
     hi('SignColumn', mod.none, bg)
 
     -- visual
-    hi('Visual', mod.none, colors.ui.dark, mod.bold)
-    hi('VisualNOS', mod.none, colors.ui.dark, mod.bold)
+    hi('Visual', mod.none, colors.ui, mod.bold)
+    hi('VisualNOS', mod.none, colors.ui, mod.bold)
 
     -- Pmenu
-    hi('Pmenu', colors.white.light, colors.black.light)
-    hi('PmenuSel', colors.white.light, colors.ui.light, mod.bold)
-    hi('PmenuSbar', mod.none, colors.white.dark)
-    hi('PmenuThumb', mod.none, colors.ui.light)
+    hi('Pmenu', colors.light_white, colors.light_black)
+    hi('PmenuSel', colors.light_white, colors.ui, mod.bold)
+    hi('PmenuSbar', mod.none, colors.dark_white)
+    hi('PmenuThumb', mod.none, colors.ui)
 
     -- folds
-    hi('FoldColumn', colors.white.dark, bg)
-    hi('Folded', colors.white.dark, bg, mod.italic)
+    hi('FoldColumn', colors.dark_white, bg)
+    hi('Folded', colors.dark_white, bg, mod.italic)
 
     -- search
-    hi('IncSearch', colors.black.light, colors.warning.dark, mod.bold)
-    hi('Search', colors.white.light, colors.ui.dark)
-    hi('MatchParen', mod.none, colors.ui.dark, mod.bold)
+    hi('IncSearch', colors.light_black, colors.warning, mod.bold)
+    hi('Search', colors.light_white, colors.ui)
+    hi('MatchParen', mod.none, colors.ui, mod.bold)
 
     -- diagnostics
-    hi('SpellBad', colors.warning.dark, bg, mod.underline)
+    hi('SpellBad', colors.warning, bg, mod.underline)
     hilink('SpellCap', 'SpellBad')
     hilink('SpellLocal', 'SpellBad')
     hilink('SpellRare', 'SpellBad')
 
     -- diff
-    hi('DiffAdd', colors.black.light, colors.green.light)
-    hi('DiffChange', colors.black.light, colors.yellow.light)
-    hi('DiffDelete', colors.black.light, colors.red.light)
+    hi('DiffAdd', colors.light_black, colors.light_green)
+    hi('DiffChange', colors.light_black, colors.light_yellow)
+    hi('DiffDelete', colors.light_black, colors.light_red)
     hilink('DiffText', 'Normal')
     hilink('diffAdded', 'DiffAdd')
     hilink('diffChanged', 'DiffChange')
@@ -269,11 +269,11 @@ local function set_gui(colors, bg)
     hilink('diffLine', 'DiffText')
 
     -- other
-    hi('Directory', colors.ui.light, bg)
+    hi('Directory', colors.ui, bg)
 end
 
 local function set_highlights(colors)
-    hi('Normal', colors.white.light, colors.bg)
+    hi('Normal', colors.light_white, colors.bg)
 
     set_native_syntax(colors, colors.bg)
     set_visual_elements(colors, colors.bg)
