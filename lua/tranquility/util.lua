a8b132f3182ff75b56387d77c7ac73fc069b017d
Jan Ouwens <jan.ouwens@gmail.com>
2024-01-30 15:11:31 +0100

    Moves to native lua highlighting

--- lua/tranquility/util.lua
+++ lua/tranquility/util.lua
@@ -6,24 +6,13 @@ local mod = {
     strikethrough = 'strikethrough'
 }
 
-local function hi(group, fg, bg, gui)
-    local cmd = 'highlight ' .. group
-    if fg ~= nil then
-        cmd = cmd .. ' guifg=' .. fg
+local function hi(group, param)
+    if type(param) == 'string' then
+        vim.api.nvim_set_hl(0, group, { link = param })
+        return
     end
-    if bg ~= nil then
-        cmd = cmd .. ' guibg=' .. bg
-    end
-    if gui == nil then
-        gui = mod.none
-    end
-    cmd = cmd .. ' gui=' .. gui
-    vim.cmd(cmd)
+
+    vim.api.nvim_set_hl(0, group, param)
 end
 
-local function hilink(group, link)
-    local cmd = string.format('highlight! link %s %s', group, link)
-    vim.cmd(cmd)
-end
-
-return { mod = mod, hi = hi, hilink = hilink }
+return { mod = mod, hi = hi }
